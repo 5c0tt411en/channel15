cce0251a093bf584acf0d19e0882d38713fafa3e
scott <5c0tt.411en@gmail.com>
2016-06-21 23:07:48 +0900

    updated

--- channel15/src/v01.cpp
+++ channel15/src/v01.cpp
@@ -13,6 +13,8 @@ v01::v01(){
 }
 
 void v01::setup(){
+    cam.setPosition(ofGetWidth() / 2, ofGetHeight() / 2, 100);
+    pointToView = ofVec3f(ofGetWidth() / 2, ofGetHeight() / 2, 0);
     ofSetLogLevel(OF_LOG_VERBOSE);
     shader.setGeometryInputType(GL_LINES);
     shader.setGeometryOutputType(GL_POLYGON);
@@ -22,7 +24,7 @@ void v01::setup(){
     for(int i = 0; i < pointSize; i++) {
         rbPt.x = ofNoise(i * noiseSpeedX) * noiseRateX;
         rbPt.y = ofNoise(i * noiseSpeedY) * noiseRateY;
-        rbPt.z = -i * speed * (1 + (a04 + c04 * audioLevel01)) + pointSize / 20;
+        rbPt.z = -i * speed * (0.1 + (a04 + c04 * audioLevel01)) + pointSize / 20;
         rbPt.x = ofMap(rbPt.x, 0, 1, ofGetWidth() / 2 + r * 0.1 * sin(i * rotateRate), ofGetWidth() / 2 + r * sin(i * rotateRate));
         rbPt.y = ofMap(rbPt.y, 0, 1, ofGetHeight() / 2 + r * 0.1 * cos(i * rotateRate), ofGetHeight() / 2 + r * cos(i * rotateRate));
         points.push_back(rbPt);
@@ -33,11 +35,18 @@ void v01::setup(){
 }
 
 void v01::update(){
+    pointToView.x = ofGetWidth() / 2 + 100 * sin(ofGetElapsedTimef() * 0.5);
+    pointToView.y = ofGetHeight() / 2 + 100 * sin(ofGetElapsedTimef() * 0.35);
+    pointToView.z = 100 * sin(ofGetElapsedTimef() * 0.4);
+    
+    node.setPosition(pointToView);
+    node.setOrientation(ofVec3f(ofGetWidth() / 2 + 100, ofGetHeight() / 2, -100));
+    
     points.erase(points.begin());
     for(int i = pointSize - 1; i < pointSize; i++) {
         rbPt.x = ofNoise(noiseSpeedX * (i + ofGetFrameNum())) * noiseRateX;
         rbPt.y = ofNoise(noiseSpeedY * (i + ofGetFrameNum())) * noiseRateY;
-        rbPt.z = -speed * (1 + (a04 + c04 * audioLevel01)) * (i + ofGetFrameNum());
+        rbPt.z = -speed * (0.1 + (a04 + c04 * audioLevel01)) * (i + ofGetFrameNum());
         rbPt.x = ofMap(rbPt.x, 0, 1, ofGetWidth() / 2 + r * 0.1 * sin(rotateRate * (i + ofGetFrameNum())), ofGetWidth() / 2 + r * sin(rotateRate * (i + ofGetFrameNum())) * (1 + (a01 + c01 * audioLevel01)));
         rbPt.y = ofMap(rbPt.y, 0, 1, ofGetHeight() / 2 + r * 0.1 * cos(rotateRate * (i + ofGetFrameNum())), ofGetHeight() / 2 + r * cos(rotateRate * (i + ofGetFrameNum())) * (1 + (a02 + c02 * audioLevel01)));
         points.push_back(rbPt);
@@ -45,7 +54,17 @@ void v01::update(){
 }
 
 void v01::draw(){
-//    cam.begin();
+    cam.lookAt(node.getPosition());
+    cam.begin();
+    ofDrawAxis(1000);
+    ofSetColor(255, 0, 0);
+    cam.draw();
+    ofSetColor(0, 255, 0);
+    node.draw();
+    ofSetColor(0, 255, 255);
+    ofDrawLine(cam.getPosition(),node.getPosition());
+    
+    
     ofNoFill();
     ofSetColor(100, 160, 120);
     ofPushMatrix();
@@ -58,11 +77,15 @@ void v01::draw(){
         // make light direction slowly rotate
         shader.setUniform3f("lightDir", sin(ofGetElapsedTimef()/10), cos(ofGetElapsedTimef()/10), 0);
     }
-    ofTranslate(0, 0, ofGetFrameNum() * speed * (1 + (a04 + c04 * audioLevel01)) + pointSize);
+    ofTranslate(0, 0, ofGetFrameNum() * speed * (0.1 + (a04 + c04 * audioLevel01)) + pointSize);
     for(unsigned int i=1; i<points.size(); i++) {
         ofDrawLine(points[i-1], points[i]);
+        if (i > 5) ofDrawLine(points[i-5], points[i]);
+        if (i > 7) ofDrawLine(points[i-7], points[i]);
     }
     if(doShader) shader.end();
     ofPopMatrix();
-//    cam.end();
+    
+    // restore view to previous state (default openFrameworks view)
+    cam.end();
 }
--- channel15/src/v01.h
+++ channel15/src/v01.h
@@ -13,28 +13,32 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+
 class v01 {
 public:
     v01();
     void setup();
     void update();
     void draw();
+    void reset();
     
     ofShader shader;
     bool doShader;
     vector<ofVec3f> points;
     ofVec3f rbPt;
-    int pointSize     = 5000,
+    int pointSize     = 500,
         r             = 300,
         lineWidth     = 100;
-    float speed       = 10,
-          rotateRate  = 5,
+    float speed       = 50,
+          rotateRate  = 1,
           noiseRateX  = 1,
           noiseRateY  = 1.2,
           noiseSpeedX = 0.6,
           noiseSpeedY = 0.75;
     
-//    ofEasyCam cam;
+    ofCamera cam;
+    ofVec3f pointToView;
+    ofNode node;
 };
 
 #endif /* defined(__channel15__v01__) */
